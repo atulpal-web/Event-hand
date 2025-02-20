2f948a02cd6684f32f4b4f4db09675a65d8d3b30
atulpal-web <palatul8141@gmail.com>
2025-02-20 09:50:18 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import ClassCounter from "./ClassComponent/ClassCounter"
-import About from "./pages/About"
-import Home from "./pages/Home"
-import ClassProps from "./ClassComponent/ClassProps.jsx"
-import RenderApi from "./hook/RenderApi.jsx"
-import Counter from "./hook/Counter.jsx"
-import OnClick from "./hook/OnClick.jsx"
-import Form from "./HandleForm/Form.jsx"
-import SingleFunction from "./HandleForm/SingleFunctionForm.jsx"
+
 import ReactHookForm from "./HandleForm/ReactHookForm.jsx"
-import FinalCounter from "./HOC/FinalCounter.jsx"
+
 
 
 
 const App = () => {
     return (
         <>
-            {/* <Home /> 
-            <ClassCounter/>
-            <Counter/>  
-            <ClassProps /> 
-            <RenderApi/>
-            <OnClick/>
-            <Form/> */}
-            {/* <SingleFunction/>
-            <FinalCounter/> */}
+          
             <ReactHookForm/>
 
 
